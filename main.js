8e4a9aacf9034a652a2d66c5770c9405e10bda6a
Miguel <114187363+MiguelFAB@users.noreply.github.com>
2023-08-20 23:11:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,119 @@
-x = 0;
-y = 0;
-drawCircle = "";
-drawRect = "";
 
-var SpeechRecognition = window.webkitSpeechRecognition;
+var canvas = new fabric.Canvas('myCanvas');
 
-var Recognition = new SpeechRecognition();
+ball_y=0;
+ball_x=0;
+hole_y=400;
+hole_x=800;
 
-function start(){
-    document.getElementById("status").innerHTML = "a IA está te ouvindo:";
-    Recognition.start();
+
+block_image_width = 5;
+block_image_height = 5;
+
+function loadImg(){
+	fabric.Image.fromURL("golf-h.png", function(Img){
+		hole_obj = Img;
+		hole_obj.scaleToWidth(50);
+		hole_obj.scaleToHeight(50);
+		hole_obj.set({
+			top:hole_y,
+			left:hole_x
+		});
+		canvas.add(hole_obj);
+		});
+	newImage();
 }
 
-Recognition.onresult = function(event){
-    console.log(event);
-    var content = event.results[0][0].transcript;
-    document.getElementById("status").innerHTML = "A Fala Foi Entendida Pela IA desta Maneira:" + content ;
-    if(content == "círculo"){
-        x = Math.floor(Math.random()*900);
-        y = Math.floor(Math.random()*600);
-        document.getElementById("status").innerHTML = "Escrevendo Um Círculo" ; 
-        drawCircle = "set";
-    };
-    if(content == "retângulo"){
-        x = Math.floor(Math.random()*900);
-        y = Math.floor(Math.random()*600);
-        document.getElementById("status").innerHTML = "Escrevendo Um Retangulo" ; 
-        drawRect = "set";
-    };
+function newImage()
+{
+	fabric.Image.fromURL("ball.png", function(Img) {
+	ball_obj = Img;
+	ball_obj.scaleToWidth(50);
+	ball_obj.scaleToHeight(50);
+	ball_obj.set({
+		top:ball_y,
+		left:ball_x
+	});
+	canvas.add(ball_obj);
+	});	
 };
-function setup(){
-    canvas = createCanvas(900, 600);
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	if((ball_x==hole_x)&&(ball_y==hole_y)){
+		canvas.remove(ball_obj);
+		document.getElementById("hd3").innerHTML="você atingiu o objetivo!!!";
+		document.getElementById("myCanvas").style.borderColor="red";
+	}
+	else{
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
 }
-function draw(){
-    if(drawCircle == "set"){
-        radius = Math.floor(Math.random()*100);
-        circle(x, y ,radius); 
-        document.getElementById("status").innerHTML = "Círculo Desenhando" ;
-        drawCircle = ""; 
+
+function up()
+{
+	ball_y = ball_y - block_image_height;
+	console.log("Altura Da Imagem Do Bloco = " + block_image_height);
+	console.log("Quando a tecla direcional cima é pressionada, X = " + ball_x + " , Y = " + ball_y);
+	canvas.remove(ball_obj);
+	newImage();
+}
+
+function down()
+{
+		if(ball_y <=450)
+		{		 
+		ball_y = ball_y + block_image_height;
+		console.log("Altura Da Imagem Do Bloco t = " + block_image_height);
+		console.log("Quando a tecla direcional baixo é pressionada,X  = " + ball_x + " , Y = "+ball_y);
+		canvas.remove(ball_obj);
+		newImage()
+		}
+}
+
+function left()
+{
+    if(ball_x > 5)  // Corrected from ball_X to ball_x
+    {
+        ball_x = ball_x - block_image_width;
+        console.log("Largura Da Imagem Do Bloco = " + block_image_width);
+        console.log("Quando a tecla direcional esquerda é pressionada, X = " + ball_x + " , Y = " + ball_y);
+        canvas.remove(ball_obj);
+        newImage();
     }
-    if(drawRect == "set"){
-        rect(x, y , 70, 50); 
-        document.getElementById("status").innerHTML = "Retángulo Desenhando" ;
-        drawRect = ""; 
-    };
-};
+}
+
+function right()
+{
+    if(ball_x <= 1050)  // Corrected from ball_X to ball_x
+    {
+        ball_x = ball_x + block_image_width;
+        console.log("Largura Da Imagem Do Bloco = " + block_image_width);
+        console.log("Quando a tecla direcional direita é pressionada, X = " + ball_x + " , Y = " + ball_y);
+        canvas.remove(ball_obj);
+        newImage();
+    }
+}
